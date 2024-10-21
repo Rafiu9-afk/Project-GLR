b2816636a78ee426532f3db6fa75cfdec2bc5af5
Kaan Bekdas <102372@glr.nl>
2024-10-21 09:39:16 +0000

    almost fully finished

--- kaan/main.js
+++ kaan/main.js
@@ -10,5 +10,5 @@ function startGame() {
 }
 function goToMainPage() {
     // Replace 'main-page.html' with the actual main page URL
-    window.location.href ='/Main page/index.html';
+    window.location.href ='mainpage.html';
 }
