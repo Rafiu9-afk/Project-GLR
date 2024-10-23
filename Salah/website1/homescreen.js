38f1e222e05d2d440b581d1405b1382224763d55
Salahedin90 <102454@glr.nl>
2024-10-23 11:25:26 +0000

    puntjes op de i gezet

--- Salah/website1/homescreen.js
+++ Salah/website1/homescreen.js
@@ -2,7 +2,7 @@
 document.querySelectorAll('.options li').forEach(item => {
     item.addEventListener('click', function() {
         if (!item.classList.contains('disabled')) {
-            alert('Selected: ' + item.textContent);
+           
         }
     });
 });
